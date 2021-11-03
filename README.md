9b0ff91353cda2c20cac4a6e672371a56e21485c
wanyx <1656026361@qq.com>
2021-11-03 16:18:00 +0800

    clean code

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # toolbox
 toolbox: mkpasswd...
+
+
+## Install
+
+```shell
+go install github.com/wanyxkhalil/toolbox@latest
+toolbox -h
+```
+
+### Completion
+
+If you use zsh, add this to ~/.zshrc & source.
+Also support bash, fish, powershell 
+```shell
+if [ $commands[toolbox] ]; then
+	source <(toolbox completion zsh)
+	compdef _toolbox toolbox
+fi
+```
+
+## Usage
+
+### mkpasswd
+
+Generate password
+```shell
+$ toolbox mkpasswd -h # help message
+A tool for generating random passwords
+
+Usage:
+  toolbox mkpasswd [flags]
+
+Flags:
+  -d, --digit uint     Number of digits (default 2)
+  -h, --help           help for mkpasswd
+  -l, --length uint    Length in chars (default 9)
+  -c, --lower uint     Number of lowercase chars (default 2)
+  -s, --special uint   Number of special chars
+  -C, --upper uint     Number of uppercase chars (default 2)
+```
+
+Sample
+```shell
+toolbox mkpasswd -l 17 # length is 17
+toolbox mkpasswd -l 17 -C 4 -d 4 -s 3 # length is 17, include 4 upper char, 4 digit, 3 special char, 6 lower char
+```
--- cmd/root.go
+++ cmd/root.go
@@ -1,65 +1,19 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
-	"os"
-
-	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "toolbox",
-	Short: "Khalil toolbox",
-	Long: `Khalil toolbox: mkpasswd`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Use:       "toolbox",
+	Short:     "Khalil toolbox",
+	Long:      `Khalil toolbox: mkpasswd`,
+	ValidArgs: []string{"mkpasswd"},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
-}
-
-func init() {
-	//cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	//rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.toolbox.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".toolbox" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".toolbox")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
